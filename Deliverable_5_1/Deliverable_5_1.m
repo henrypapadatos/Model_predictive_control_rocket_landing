8f13de89e8a7cb621e9ee5d735005e15b58c18d8
Aitana <aitana.waelbroeck@hotmail.com>
2021-12-17 13:43:59 +0100

    changes part 5

--- Deliverable_5_1/Deliverable_5_1.m
+++ Deliverable_5_1/Deliverable_5_1.m
@@ -1,3 +1,54 @@
 addpath(fullfile('..', 'src'));
+%%
+d1 = 0;
+d2 = 0;
+%  Pavg = (P1 + P2)/2, limited to [20%, 80%]
+Pavg = 0;
+%Pdiff = P2 − P1, Pdiff might be up to ±20%
+Pdiff = 0;
+w = [0 0 0];
+phi = [0 0 0];
+v = [0 0 0];
+p = [0 0 0];
 
-%% TODO: This file should produce all the plots for the deliverable
+Ts = 1/20;
+rocket = Rocket(Ts);
+u = [d1, d2, Pavg, Pdiff]'; % (Assign appropriately)
+[b_F, b_M] = rocket.getForceAndMomentFromThrust(u)
+x = [w, phi, v, p]'; % (Assign appropriately)
+%derivee de x
+x_dot = rocket.f(x, u)
+
+rocket = Rocket(Ts);
+Tf = 2.0; % Time to simulate for
+x0 = [deg2rad([2 -2 0, -2 2 0]), 0 0 0, 0 0 0]'; % (w, phi, v, p) Initial state
+u = [deg2rad([2 0]), 60, 0 ]'; % (d1 d2 Pavg Pdiff) Constant input
+[T, X, U] = rocket.simulate_f(x0, Tf, u); % Simulate nonlinear dynamics f
+rocket.anim_rate = 1.0;
+rocket.vis(T, X, U); 
+%%
+%%TO DO 5
+
+Ts = 1/20; %Sample time
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+% Design MPC controller
+H = 5; % Horizon length in seconds
+
+mpc_z = MPC_Control_z(sys_z, Ts, H);
+
+rocket.mass = 1.783; % Manipulate mass for simulation
+z_ref=1;
+z = [0;0];
+
+% Get control input
+uz = mpc_z.get_u(z,z_ref);
+
+Tf = 10;
+z0 = [0;0];
+[T, Z_sub, U_sub] = rocket.simulate(sys_z, z0, Tf, @mpc_z.get_u, z_ref);
+%[T, X, U, Ref] = rocket.simulate_f(x0, Tf, mpc, ref);
+
+
---
+++ Deliverable_5_1/MPC_Control_z.m
@@ -0,0 +1,180 @@
+classdef MPC_Control_z < MPC_Control
+    % the d only makes a difference in the constraints of the optimizer and
+    % of the sst target.
+    % how to set the poles of the new close loop system? because I'm
+    % getting an error.
+    % where is the d_estimate0 set? in rocket?
+    %should I write my observer in here or only the set up of the matrices?
+    %I can only test it by testing the whole system?
+    %is the B matrix infront of d in the system important
+    
+    properties
+        A_bar, B_bar, C_bar % Augmented system for disturbance rejection
+        L                   % Estimator gain for disturbance rejection
+    end
+    
+    methods
+        function mpc = MPC_Control_z(sys, Ts, H)
+            mpc = mpc@MPC_Control(sys, Ts, H);
+            
+            [mpc.A_bar, mpc.B_bar, mpc.C_bar, mpc.L] = mpc.setup_estimator();
+        end
+        
+        % Design a YALMIP optimizer object that takes a steady-state state
+        % and input (xs, us) and returns a control input
+        function ctrl_opti = setup_controller(mpc, Ts, H)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   X(:,1)       - initial state (estimate)
+            %   d_est        - disturbance estimate
+            %   x_ref, u_ref - reference state/input
+            % OUTPUTS
+            %   U(:,1)       - input to apply to the system
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            N = ceil(H/Ts); % Horizon steps
+            
+            [nx, nu] = size(mpc.B);
+            
+            % Targets (Ignore this before Todo 3.3)
+            x_ref = sdpvar(nx, 1);
+            u_ref = sdpvar(nu, 1);
+            
+            % Disturbance estimate (Ignore this before Part 5)
+            d_est = sdpvar(1);
+            
+            % Predicted state and input trajectories
+            X = sdpvar(nx, N);
+            U = sdpvar(nu, N-1);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            
+            % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are
+            %       the DISCRETE-TIME MODEL of your system
+            
+            % Define Q and R matrices 
+            Q = eye(nx)*5;
+            R = zeros(nu);
+            
+            % Define U constraints for (both with vectors and scalars)
+            uss = 56.6667;
+            P_avg_lim_low = 50 - uss;
+            P_avg_lim_high = 80 - uss;
+            M = [1;-1];
+            m = [P_avg_lim_high;-P_avg_lim_low];
+            
+            % Compute LQR invariant set and final cost
+            [~,P,~] = dlqr(mpc.A,mpc.B,Q,R);
+            
+            % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+            obj = 0;
+            con = [];
+            
+            for i=1:N-1
+                % Discrete Time model constraint
+                con = [con, X(:,i+1) == mpc.A*X(:,i) + mpc.B*U(:,i)+ d_est];
+                % Constraints on U
+                con = [con, M*U(:,i) <= m];
+                % Increment the objective function
+                obj = obj + (X(:,i) - x_ref)'*Q*(X(:,i) - x_ref) + (U(:,i) - u_ref)'*R*(U(:,i) - u_ref); 
+            end
+            % Increment the objective function with the final cost
+            obj = obj + (X(:,i) - x_ref)'*P*(X(:,i) - x_ref);
+            
+            % Return YALMIP optimizer object
+            ctrl_opti = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+                {X(:,1), x_ref, u_ref, d_est}, U(:,1));
+        end
+        
+        
+        % Design a YALMIP optimizer object that takes a position reference
+        % and returns a feasible steady-state state and input (xs, us)
+        function target_opti = setup_steady_state_target(mpc)
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % INPUTS
+            %   ref    - reference to track
+            %   d_est  - disturbance estimate
+            % OUTPUTS
+            %   xs, us - steady-state target
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            nx = size(mpc.A, 1);
+            nu = size(mpc.B,2); 
+            
+            % Steady-state targets
+            xs = sdpvar(nx, 1);
+            us = sdpvar(nu,1);
+            
+            % Reference position (Ignore this before Todo 3.3)
+            ref = sdpvar;
+            
+            % Disturbance estimate (Ignore this before Part 5)
+            d_est = sdpvar;
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            uss = 56.6667;
+            P_avg_lim_low = 50 - uss;
+            P_avg_lim_high = 80 - uss;
+            M = [1;-1];
+            m = [P_avg_lim_high;-P_avg_lim_low];
+            
+            % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+            % Implement these constraints
+            obj = us'*us;
+            % Steady state constraints
+            con = [mpc.A*xs + mpc.B*us == xs];
+            % Reference constraints
+            con = [con, mpc.C*xs + mpc.D*us + d_est == ref];
+            % Input constraints
+            con = [con, M*us <= m];
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            % Compute the steady-state target
+            target_opti = optimizer(con, obj, sdpsettings('solver', 'gurobi'), {ref, d_est}, {xs, us});
+        end
+        
+        
+        % Compute augmented system and estimator gain for input disturbance rejection
+        function [A_bar, B_bar, C_bar, L] = setup_estimator(mpc)
+            
+            %%% Design the matrices A_bar, B_bar, L, and C_bar
+            %%% so that the estimate x_bar_next [ x_hat; disturbance_hat ]
+            %%% converges to the correct state and constant input disturbance
+            %%%   x_bar_next = A_bar * x_bar + B_bar * u + L * (C_bar * x_bar - y);
+            
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+            [nx, nu] = size(mpc.B);
+            ny   = size(mpc.C,1);
+            
+            A_bar = [mpc.A     , zeros(nx,1);
+                    zeros(1,nx),1           ];
+            disp("size of A bar");
+            disp(size(A_bar));
+
+            B_bar = [mpc.B;zeros(1,nu)];
+            C_bar = [mpc.C,ones(ny,1)];
+            disp(A_bar);
+            disp(B_bar);
+            disp(C_bar);
+            
+            %how to define de poles?
+            L = -place(A_bar',C_bar',[0.3,0.4,0.5])';
+            
+%             x_bar = [ x_hat; disturbance_hat ];
+%             
+%             x_bar_next = A_bar * x_bar + B_bar * u + L * (C_bar * x_bar - y)
+            
+            % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        end
+        
+        
+    end
+end
