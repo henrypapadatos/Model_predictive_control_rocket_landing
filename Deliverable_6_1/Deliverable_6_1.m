f6df519c3c9af46c43c3e960679ab20270931b57
arnaudguibbert <arnaud.guibbert@epfl.ch>
2022-01-28 22:17:46 +0100

    Final version V2

--- Deliverable_6_1/Deliverable_6_1.m
+++ Deliverable_6_1/Deliverable_6_1.m
@@ -1,5 +1,9 @@
 clc; clear all; close all;
+warning('off', 'MATLAB:rmpath:DirNotFound');
+rmpath(genpath("../Deliverable_3_1"));
+rmpath(genpath("../Deliverable_5_1"));
 addpath(fullfile('..', 'src'));
+addpath(fullfile('..', "Deliverable_3_2"))
 
 %% Set the parameters of the simulations
 
@@ -11,7 +15,7 @@ Tf = 30;
 %% With 15° for the roll
 
 % Define the controller
-nmpc = NMPC_Control_roll15(rocket, H);
+nmpc = NMPC_Control(rocket, H);
 
 % MPC reference with default maximum roll = 15 deg
 ref = @(t_, x_) rocket.MPC_ref(t_, Tf);
@@ -27,7 +31,7 @@ saveas(ph.fig,"Graphs/nmpc_15_roll.svg");
 %% With 50° for the roll
 
 % Define the controller 
-nmpc = NMPC_Control_roll50(rocket, H);
+nmpc = NMPC_Control(rocket, H);
 
 % MPC reference with specified maximum roll = 50 deg
 roll_max = deg2rad(50);
@@ -41,10 +45,43 @@ ph = rocket.plotvis(T, X, U, Ref);
 ph.fig.Name = 'Nonlin. sim'; % Set a figure title
 saveas(ph.fig,"Graphs/nmpc_50_roll.svg");
 
-%% Non linear cost function - 15° for the roll
+%% Linear controller with 50° roll
+
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+% Design MPC controller
+H = 1.5; % Horizon length in seconds
+
+
+% Merge four sub−system controllers into one full−system controller
+mpc_x = MPC_Control_x(sys_x, Ts, H);
+mpc_y = MPC_Control_y(sys_y, Ts, H);
+mpc_z = MPC_Control_z(sys_z, Ts, H);
+mpc_roll = MPC_Control_roll(sys_roll, Ts, H);
+
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+
+% Setup reference function
+roll_max = deg2rad(50);
+ref = @(t_, x_) rocket.MPC_ref(t_, Tf, roll_max);
+x0 = zeros(12,1);
+
+[T, X, U, Ref] = rocket.simulate_f(x0, Tf, mpc, ref);
+
+% Plot pose
+rocket.anim_rate = 10; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
+ph.fig.Name = 'Nonlin. sim'; % Set a figure title
+
+saveas(ph.fig,"Graphs/Linear_50roll.svg");
+
+%% Absolute cost function - 15° for the roll
+
+H = 0.5;
 
 % Define the controller
-nmpc = NMPC_Control_roll15_abs_cost(rocket, H);
+nmpc = NMPC_Control_abs_cost(rocket, H);
 
 % MPC reference with default maximum roll = 15 deg
 ref = @(t_, x_) rocket.MPC_ref(t_, Tf);
@@ -56,3 +93,4 @@ rocket.anim_rate = 10; % Increase this to make the animation faster
 ph = rocket.plotvis(T, X, U, Ref);
 ph.fig.Name = 'Nonlin. sim'; % Set a figure title
 saveas(ph.fig,"Graphs/non_linear_nmpc_15_roll.svg");
+
